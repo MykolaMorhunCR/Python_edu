e41bb00ee7dea3ba05ef7b4eaa8e0b0fa81a6052
Nik <>
2020-05-27 15:49:57 +0300

    Tasks2 and 3

---
+++ Tasks23.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+input_array = np.random.randint(-1000, high=1000, size=1000)
+
+
+def find_mean(main_array):
+    mean = np.mean(main_array)
+
+    return mean
+
+
+def Bounds(main_array, n=0):
+    new_array = [None] * len(main_array)
+    upper_bound = find_mean(main_array) + (find_mean(main_array) * 0.2)
+    lower_bound = find_mean(main_array) - (find_mean(main_array) * 0.2)
+
+    for i in range(0, len(main_array)):
+        if upper_bound > main_array[i] > lower_bound:
+            new_array[n] = main_array[i]
+            n += 1
+
+    close_to_mean = list(filter(None, new_array))
+
+    return close_to_mean
+
+
+def maximum(test_array):
+    sorted_array = np.unique(np.sort(test_array))
+    reverse_sorted_array = sorted_array[::-1]
+    print(reverse_sorted_array[0], reverse_sorted_array[1])
+
+
+find_mean(input_array)
+maximum(input_array)
